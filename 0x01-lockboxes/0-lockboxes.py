edaeeb24a563aed9562fc708e2e9ac4f01eb3a6d
tobias-barakaa <tobiasbarakan@gmail.com>
2023-12-07 15:16:28 -0800

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,18 @@
+def canUnlockAll(boxes):
+    if not boxes or not boxes[0]:
+        return False
+
+    n = len(boxes)
+    visited = [False] * n
+    visited[0] = True
+    stack = [0]
+
+    while stack:
+        current_box = stack.pop()
+
+        for key in boxes[current_box]:
+            if 0 <= key < n and not visited[key]:
+                visited[key] = True
+                stack.append(key)
+
+    return all(visited)
