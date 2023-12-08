cb905e21edc3de44361052fa1d6c5a0e326615d5
Baraka Ngaira <ngairato@gmail.com>
2023-12-08 03:23:04 +0300

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,3 +1,23 @@
+#!/usr/bin/python3
+
+"""
+    Determines if all the boxes can be opened.
+
+    Args:
+    - boxes (list of lists): A list of lists where each inner list represents a box.
+                             Each box may contain keys to other boxes, and a key with the
+                             same number as a box can open that box.
+
+    Returns:
+    - bool: True if all boxes can be opened, False otherwise.
+
+    Constraints:
+    - The first box boxes[0] is assumed to be unlocked.
+    - All keys are positive integers.
+    - There can be keys that do not have corresponding boxes.
+"""
+
+
 def canUnlockAll(boxes):
     if not boxes or not boxes[0]:
         return False
