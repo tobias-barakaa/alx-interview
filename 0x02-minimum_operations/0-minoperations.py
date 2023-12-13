b4a699ad1161e1b17755dd1fd470cd52fa8db364
Baraka Ngaira <ngairato@gmail.com>
2023-12-14 01:21:56 +0300

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,19 +2,15 @@ def minOperations(n):
     if n <= 1:
         return 0
 
-    
-    min_ops = [float('inf')] * (n + 1)
+    operations = 0
+    i = 2
+    while i * i <= n:
+        while n % i == 0:
+            operations += i
+            n //= i
+        i += 1
 
-    
-    min_ops[1] = 0
+    if n > 1:
+        operations += n
 
-    
-    for i in range(2, n + 1):
-        
-        if n % i == 0:
-            for j in range(i // 2, 0, -1):
-                if i % j == 0:
-                    min_ops[i] = min(min_ops[i], min_ops[j] + (i // j))
-                    break
-
-    return min_ops[n] if min_ops[n] != float('inf') else 0
+    return operations
