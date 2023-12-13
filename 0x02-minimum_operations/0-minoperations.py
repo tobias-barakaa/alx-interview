c17b322a49c573624f8400ae75f8f586c1673d8d
Baraka Ngaira <ngairato@gmail.com>
2023-12-14 01:25:42 +0300

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,15 +1,27 @@
 def minOperations(n):
+    """
+    Calculate the minimum number of operations required to achieve 'n' characters in a file.
+
+    Parameters:
+    n (int): The target number of characters.
+
+    Returns:
+    int: The minimum number of operations required.
+    """
     if n <= 1:
         return 0
 
     operations = 0
     i = 2
+
+    
     while i * i <= n:
         while n % i == 0:
             operations += i
             n //= i
         i += 1
 
+    
     if n > 1:
         operations += n
 
