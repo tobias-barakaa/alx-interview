425c42dfd0e28448e3d0c567500f6a1de44c3f4a
tobias-barakaa <ngairato@gmail.com>
2023-12-21 12:50:40 -0800

    0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,16 +4,18 @@ import sys
 from collections import defaultdict
 
 def print_stats(total_size, status_counts):
+    """Print statistics based on total size and status counts."""
     print(f"File size: {total_size}")
     for code in sorted(status_counts.keys()):
         print(f"{code}: {status_counts[code]}")
 
 def parse_line(line):
+    """Parse a log line and extract relevant information."""
     parts = line.split()
     if len(parts) != 7:
         return None
     ip, _, _, status_code, file_size = parts[0], parts[3], parts[5], parts[6]
-    if status_code.isdigit() and int(status_code) in [200, 301, 400, 401, 403, 404, 405, 500]:
+    if status_code.isdigit() and int(status_code) in {200, 301, 400, 401, 403, 404, 405, 500}:
         return int(status_code), int(file_size)
     return None
 
