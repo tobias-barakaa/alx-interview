c55aadf0097c8913e38c96d8f05badacb7ee94ae
tobias-barakaa <ngairato@gmail.com>
2023-12-21 13:48:28 -0800

    0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,45 +1,34 @@
 #!/usr/bin/python3
 
 import sys
+import re
 from collections import defaultdict
 
-
-def print_stats(total_size, status_counts):
-    """Print statistics based on total size and status counts."""
+def print_statistics(total_size, status_counts):
     print(f"File size: {total_size}")
-    for code in sorted(status_counts.keys()):
-        print(f"{code}: {status_counts[code]}")
+    for status_code in sorted(status_counts.keys()):
+        print(f"{status_code}: {status_counts[status_code]}")
 
+def main():
+    total_size = 0
+    status_counts = defaultdict(int)
+    line_count = 0
 
-def parse_line(line):
-    """Parse a log line and extract relevant information."""
-    parts = line.split()
-    if len(parts) != 7:
-        return None
-    ip, _, _, status_code, file_size = parts[0], parts[3], parts[5], parts[6]
-    if status_code.isdigit() and int(status_code) in {200, 301, 400, 401, 403, 404, 405, 500}:
-        return int(status_code), int(file_size)
-    return None
-
-
-total_size = 0
-status_counts = defaultdict(int)
-line_count = 0
-
-try:
-    for line in sys.stdin:
-        line = line.strip()
-        parsed = parse_line(line)
-        if parsed:
-            status_code, file_size = parsed
-            total_size += file_size
-            status_counts[status_code] += 1
+    try:
+        for line in sys.stdin:
             line_count += 1
+            match = re.match(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[\S+\] "GET /projects/260 HTTP/1.1" (\d{3}) (\d+)', line)
+            if match:
+                _, status_code, file_size = match.groups()
+                total_size += int(file_size)
+                status_counts[int(status_code)] += 1
 
-        if line_count == 10:
-            print_stats(total_size, status_counts)
-            line_count = 0
+            if line_count % 10 == 0:
+                print_statistics(total_size, status_counts)
 
-except KeyboardInterrupt:
-    print_stats(total_size, status_counts)
-    sys.exit(0)
+    except KeyboardInterrupt:
+        print_statistics(total_size, status_counts)
+        sys.exit(0)
+
+if __name__ == "__main__":
+    main()
