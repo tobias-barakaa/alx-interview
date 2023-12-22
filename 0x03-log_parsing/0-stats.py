4af2519891ae4d380aafd1d854ddb5d8c72723d4
Baraka Ngaira <ngairato@gmail.com>
2023-12-22 04:57:40 +0300

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,34 +1,46 @@
 #!/usr/bin/python3
+"""
+Log parsing
+"""
 
 import sys
-import re
-from collections import defaultdict
 
-def print_statistics(total_size, status_counts):
-    print(f"File size: {total_size}")
-    for status_code in sorted(status_counts.keys()):
-        print(f"{status_code}: {status_counts[status_code]}")
+def initialize_stats() -> dict:
+    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    return {k: 0 for k in codes}
+
+def parse_line(line: str) -> tuple:
+    try:
+        data = line.split()
+        status_code = data[-2]
+        file_size = int(data[-1])
+        return status_code, file_size
+    except (IndexError, ValueError):
+        return None, 0
+
+def print_stats(stats: dict, file_size: int) -> None:
+    print("File size: {:d}".format(file_size))
+    for k, v in sorted(stats.items()):
+        if v:
+            print("{}: {}".format(k, v))
 
 def main():
-    total_size = 0
-    status_counts = defaultdict(int)
-    line_count = 0
+    filesize, count = 0, 0
+    stats = initialize_stats()
 
     try:
         for line in sys.stdin:
-            line_count += 1
-            match = re.match(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[\S+\] "GET /projects/260 HTTP/1.1" (\d{3}) (\d+)', line)
-            if match:
-                _, status_code, file_size = match.groups()
-                total_size += int(file_size)
-                status_counts[int(status_code)] += 1
-
-            if line_count % 10 == 0:
-                print_statistics(total_size, status_counts)
-
+            count += 1
+            status_code, file_size = parse_line(line)
+            if status_code in stats:
+                stats[status_code] += 1
+            filesize += file_size
+            if count % 10 == 0:
+                print_stats(stats, filesize)
+        print_stats(stats, filesize)
     except KeyboardInterrupt:
-        print_statistics(total_size, status_counts)
-        sys.exit(0)
+        print_stats(stats, filesize)
+        raise
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
