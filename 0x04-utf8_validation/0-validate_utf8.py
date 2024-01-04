3be561c6d200f2ca2412ebea7b7246d6d666b53a
tobias-barakaa <ngairato@gmail.com>
2024-01-04 13:19:03 -0800

    0-validate_utf8.py

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+utf-8 validation
+"""
+
+
+def validUTF8(data):
+    """
+    method that determines if a given data set represents a valid UTF-8 encoding
+    """
+    n_bytes = 0
+    for num in data:
+        bin_rep = format(num, '#010b')[-8:]
+        if n_bytes == 0:
+            for bit in bin_rep:
+                if bit == '0':
+                    break
+                n_bytes += 1
+            if n_bytes == 0:
+                continue
+            if n_bytes == 1 or n_bytes > 4:
+                return False
+        else:
+            if not (bin_rep[0] == '1' and bin_rep[1] == '0'):
+                return False
+        n_bytes -= 1
+    return n_bytes == 0
