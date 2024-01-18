2054c793e70dd3d529876db18edf5afd33239336
tobias-barakaa <ngairato@gmail.com>
2024-01-18 14:49:36 -0800

    0-starwars_characters.js

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+if (process.argv.length !== 3) {
+  console.error('Usage: ./0-starwars_characters.js <movie_id>');
+  process.exit(1);
+}
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+request(apiUrl, function (error, response, body) {
+  if (error) {
+    console.error('Error:', error);
+  } else if (response.statusCode !== 200) {
+    console.error('Invalid response:', response.statusCode);
+  } else {
+    const filmData = JSON.parse(body);
+    const characters = filmData.characters;
+
+    characters.forEach((characterUrl) => {
+      request(characterUrl, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+          const characterData = JSON.parse(body);
+          console.log(characterData.name);
+        } else {
+          console.error('Error fetching character:', error);
+        }
+      });
+    });
+  }
+});
