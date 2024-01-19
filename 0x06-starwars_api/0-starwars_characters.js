5de6513a3318f6bb93cf273e4ef0f2c26a552d08
Baraka Ngaira <ngairato@gmail.com>
2024-01-19 05:23:35 +0300

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,19 +1,28 @@
 #!/usr/bin/node
 
+const { promisify } = require('util');
+const request = promisify(require('request'));
 const movieId = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-async function fetchCharacterNames() {
+async function fetchCharacterData(characterId) {
   try {
-    const response = await fetch(url);
-    const filmData = await response.json();
-
-    for (const characterUrl of filmData.characters) {
-      const characterResponse = await fetch(characterUrl);
-      const characterData = await characterResponse.json();
-      console.log(characterData.name);
-    }
+    const { body } = await request(characterId);
+    console.log(JSON.parse(body).name);
   } catch (error) {
-    console.error('Error fetching character data:', error);
+    console.error(`Error fetching character: ${error.message}`);
   }
 }
+
+(async () => {
+  try {
+    const { body } = await request(url);
+    const characters = JSON.parse(body).characters;
+
+    for (const characterId of characters) {
+      await fetchCharacterData(characterId);
+    }
+  } catch (error) {
+    console.error(`Error fetching movie data: ${error.message}`);
+  }
+})();
