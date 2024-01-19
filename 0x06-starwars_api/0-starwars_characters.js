befa2bbf3fd8bee31abcc75e29eb42af8c02a241
Baraka Ngaira <ngairato@gmail.com>
2024-01-19 05:19:52 +0300

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,21 +1,26 @@
 #!/usr/bin/node
 
 const request = require('request');
+
 const movieId = process.argv[2];
+
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-request(url, async (err, response, body) => {
-  if (err) {
-    console.log(err);
-  }
-  for (const characterId of JSON.parse(body).characters) {
-    await new Promise((resolve, reject) => {
-      request(characterId, (err, response, body) => {
-        if (err) {
-          reject(err);
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const filmData = JSON.parse(body);
+    const charactersUrls = filmData.characters;
+
+    charactersUrls.forEach(characterUrl => {
+      request(characterUrl, (error, response, body) => {
+        if (error) {
+          console.error(error);
+        } else {
+          const characterData = JSON.parse(body);
+          console.log(characterData.name);
         }
-        console.log(JSON.parse(body).name);
-        resolve();
       });
     });
   }
