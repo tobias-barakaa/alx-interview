9228c9f80cc87edf6b7dfe3b94f88a4104017d7b
Baraka Ngaira <ngairato@gmail.com>
2024-01-19 05:35:08 +0300

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,24 +1,28 @@
 #!/usr/bin/node
 
-const request = require('request');
-
+const { promisify } = require('util');
+const request = promisify(require('request'));
 const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+    
+async function fetchCharacterData(characterId) {
+  try {
+    const { body } = await request(characterId);
+    console.log(JSON.parse(body).name);
+  } catch (error) {
+    console.error(`Error fetching character: ${error.message}`);
+  }
+}
+    
+(async () => {
+  try {
+    const { body } = await request(url);
+    const characters = JSON.parse(body).characters;
 
-const url = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
-
-request(url, function (error, response, body) {
-    if (error) {
-        console.log(error);
-    } else {
-        const characters = JSON.parse(body).characters;
-        for (const character of characters) {
-        request(character, function (error, response, body) {
-            if (error) {
-            console.log(error);
-            } else {
-            console.log(JSON.parse(body).name);
-            }
-        });
-        }
+    for (const characterId of characters) {
+      await fetchCharacterData(characterId);
     }
-    });
+  } catch (error) {
+    console.error(`Error fetching movie data: ${error.message}`);
+  }
+})();
