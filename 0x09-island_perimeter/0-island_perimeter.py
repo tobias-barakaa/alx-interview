cba826df965ea5f6ba80d60b7347770d88c45d54
tobias-barakaa <ngairato@gmail.com>
2024-02-08 20:07:16 +0300

    0-island_perimeter.py

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" 
+island_perimeter module
+"""
+
+def island_perimeter(grid):
+    """
+    Returns the perimeter of the island described in grid
+    """
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+    return perimeter
