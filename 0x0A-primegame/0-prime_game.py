62e0e7a965f32b08525d28171283ab9fbe003054
tobias-barakaa <ngairato@gmail.com>
2024-02-15 16:23:01 +0300

    0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,32 +1,30 @@
 #!/usr/bin/python3
 """
-Prime Game"""
-
-
-def is_prime(num):
-    if num < 2:
-        return False
-    for i in range(2, int(num ** 0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
+Prime Game to check the prime numbers"""
 
 def isWinner(x, nums):
-    def get_winner(n):
-        if n < 2:
-            return None
-        primes_count = sum(1 for i in range(2, n + 1) if is_prime(i))
-        return "Maria" if primes_count % 2 == 0 else "Ben"
-    
-    winner_count = {"Maria": 0, "Ben": 0}
-    for n in nums:
-        winner = get_winner(n)
-        if winner:
-            winner_count[winner] += 1
-    
-    if winner_count["Maria"] > winner_count["Ben"]:
-        return "Maria"
-    elif winner_count["Maria"] < winner_count["Ben"]:
-        return "Ben"
-    else:
+    """Prime Game to check the prime numbers
+    Args:
+        x: the number of rounds
+        nums: the list of numbers
+    Returns:
+        the winner of the game
+    """
+    if nums is None or x < 1:
         return None
+    n = max(nums)
+    primes = [True for i in range(n + 1)]
+    primes[0] = primes[1] = False
+    for i in range(2, int(n ** 0.5) + 1):
+        if primes[i]:
+            for j in range(i * i, n + 1, i):
+                primes[j] = False
+    primes = [i for i, prime in enumerate(primes) if prime]
+    primes = primes[1:]
+    count = 0
+    for i in nums:
+        if i in primes:
+            count += 1
+    if count % 2 == 0:
+        return "Ben"
+    return "Maria"
