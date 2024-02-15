0bcaf61234c8a04b92d1f35f78cd871345f5360c
tobias-barakaa <ngairato@gmail.com>
2024-02-16 01:13:28 +0300

    0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,20 +4,22 @@ Prime Game to check the prime numbers"""
 
 
 def isWinner(x, nums):
-    """Prime Game to check the prime numbers
-    """
+    """Prime Game to check the prime numbers"""
     if not nums or x < 1:
         return None
     n = max(nums)
-    primes = [False, False] + [True for i in range(n - 1)]
-    for i in range(2, int(n ** 0.5) + 1):
-        if primes[i]:
-            for j in range(i * i, n + 1, i):
-                primes[j] = False
+    primes = [True for _ in range(max(n + 1, 2))]
+    primes[0:2] = [False, False]
+    for i in range(2, int(n**0.5) + 1):
+        if not primes[i]:
+            continue
+        for j in range(i*i, n + 1, i):
+            primes[j] = False
     primes = [i for i, j in enumerate(primes) if j]
-    p1 = 0
+    primes = primes[1:]
+    count = 0
     for i in nums:
-        p1 += sum([1 for j in primes if j <= i])
-    if p1 % 2 == 0:
+        count += sum(j <= i for j in primes)
+    if count % 2 == 0:
         return "Ben"
     return "Maria"
