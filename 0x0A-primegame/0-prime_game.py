9525c8fbadd5c94eb895c29a639c9a48f1a6ebc9
tobias-barakaa <ngairato@gmail.com>
2024-02-16 02:08:43 +0300

    my name is tobby

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,46 +1,28 @@
 #!/usr/bin/python3
 """
-Prime Game to check the prime numbers"""
+Prime Game
+"""
 
 
 def isWinner(x, nums):
-    """Prime Game to check the prime numbers
-    Args:
-        x: the number of rounds
-        nums: the list of numbers
-    Returns:
-        the winner of the game
     """
-    if nums is None or x < 1:
+    Prime Game
+    """
+    if not nums or x < 1:
         return None
 
-    def calculate_primes(n):
-        primes = [True for i in range(n + 1)]
-        primes[0] = primes[1] = False
-        for i in range(2, int(n ** 0.5) + 1):
-            if primes[i]:
-                for j in range(i * i, n + 1, i):
-                    primes[j] = False
-        return [i for i, prime in enumerate(primes) if prime][1:]
+    n = max(nums)
+    primes = [False, False] + [True for i in range(n - 1)]
+    for i in range(2, int(n ** 0.5) + 1):
+        if primes[i] is True:
+            for j in range(i ** 2, n + 1, i):
+                primes[j] = False
 
-    def play_game(n, primes):
-        count = 0
-        for i in range(n):
-            if i in primes:
-                count += 1
-        return count % 2 == 0
+    primes = [i for i, j in enumerate(primes) if j is True]
 
-    wins = {"Maria": 0, "Ben": 0}
-    for n in nums:
-        primes = calculate_primes(n)
-        if play_game(n, primes):
-            wins["Ben"] += 1
-        else:
-            wins["Maria"] += 1
+    count = 0
+    for i in nums:
+        if i in primes:
+            count += 1
 
-    if wins["Maria"] > wins["Ben"]:
-        return "Maria"
-    elif wins["Maria"] < wins["Ben"]:
-        return "Ben"
-    else:
-        return None
+    return "Ben" if count % 2 == 0 else "Maria"
