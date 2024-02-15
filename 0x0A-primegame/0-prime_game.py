3e504e3c9ec4790db3c7dc82dc260010537c3573
tobias-barakaa <ngairato@gmail.com>
2024-02-15 16:14:56 +0300

    0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,43 +3,30 @@
 Prime Game"""
 
 
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
 def isWinner(x, nums):
-    def is_prime(num):
-        if num <= 1:
-            return False
-        if num == 2:
-            return True
-        if num % 2 == 0:
-            return False
-        for i in range(3, int(num**0.5) + 1, 2):
-            if num % i == 0:
-                return False
-        return True
-
-    def find_next_prime(start, nums):
-        for num in range(start, max(nums) + 1):
-            if is_prime(num) and num in nums:
-                return num
+    def get_winner(n):
+        if n < 2:
+            return None
+        primes_count = sum(1 for i in range(2, n + 1) if is_prime(i))
+        return "Maria" if primes_count % 2 == 0 else "Ben"
+    
+    winner_count = {"Maria": 0, "Ben": 0}
+    for n in nums:
+        winner = get_winner(n)
+        if winner:
+            winner_count[winner] += 1
+    
+    if winner_count["Maria"] > winner_count["Ben"]:
+        return "Maria"
+    elif winner_count["Maria"] < winner_count["Ben"]:
+        return "Ben"
+    else:
         return None
-
-    wins = {'Maria': 0, 'Ben': 0}
-
-    for i in range(x):
-        turn = 'Maria'
-        current_nums = list(range(1, nums[i] + 1))
-
-        while True:
-            prime = find_next_prime(2, current_nums)
-            if prime is None:
-                break
-            current_nums = [num for num in current_nums if num % prime != 0]
-            turn = 'Ben' if turn == 'Maria' else 'Maria'
-
-        if turn == 'Maria':
-            wins['Maria'] += 1
-        else:
-            wins['Ben'] += 1
-
-    if wins['Maria'] == wins['Ben']:
-        return None
-    return max(wins, key=wins.get)
