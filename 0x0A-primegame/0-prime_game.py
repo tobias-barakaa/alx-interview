f190610d9a8c7b80d076b685a5360d2ff38dc85f
tobias-barakaa <ngairato@gmail.com>
2024-02-16 02:01:06 +0300

    hope it works

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,31 +1,35 @@
 #!/usr/bin/python3
-"""
-Prime Game to check the prime numbers"""
-
 
 def isWinner(x, nums):
-    """Prime Game to check the prime numbers
-    Args:
-        x: the number of rounds
-        nums: the list of numbers
-    Returns:
-        the winner of the game
     """
-    if nums is None or x < 1:
-        return None
-    n = max(nums)
-    primes = [True for i in range(n + 1)]
-    primes[0] = primes[1] = False
-    for i in range(2, int(n ** 0.5) + 1):
-        if primes[i]:
-            for j in range(i * i, n + 1, i):
-                primes[j] = False
-    primes = [i for i, prime in enumerate(primes) if prime]
-    primes = primes[1:]
-    count = 0
-    for i in nums:
-        if i in primes:
-            count += 1
-    if count % 2 == 0:
+    Determine the winner of the prime game.
+    
+    Args:
+        x (int): The number of rounds.
+        nums (list): An array of n for each round.
+
+    Returns:
+        str or None: Name of the player that won the most rounds, or None if winner cannot be determined.
+    """
+    def is_prime(num):
+        if num < 2:
+            return False
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                return False
+        return True
+
+    def get_winner(round_nums):
+        count = sum(1 for num in round_nums if is_prime(num))
+        return "Maria" if count % 2 != 0 else "Ben"
+
+    winners = [get_winner(nums[i]) for i in range(x)]
+    maria_wins = winners.count("Maria")
+    ben_wins = winners.count("Ben")
+
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
         return "Ben"
-    return "Maria"
+    else:
+        return None
