e50c9ae1ba0d55b1588577872585fc1cac69634a
tobias-barakaa <ngairato@gmail.com>
2024-02-16 03:31:47 +0300

    completed

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,25 +4,18 @@
     
 
 def isWinner(x, nums):
-    """ Prime Game """
-    if not nums or x < 1:
+    """ Game"""
+    x = 0
+    y = 0
+    if x == 0 or x == 1:
         return None
-    n = max(nums)
-    sieve = [True] * (n + 1)
-    sieve[0:2] = [False, False]
-    for i in range(2, int(n ** 0.5) + 1):
-        if sieve[i]:
-            for j in range(i * i, n + 1, i):
-                sieve[j] = False
-    sieve = [i for i in range(n + 1) if sieve[i]]
-    m = len(sieve)
-    dp = [0] * (m + 1)
-    for i in range(m - 1, -1, -1):
-        dp[i] = dp[i + 1]
-        if sieve[i] in nums:
-            dp[i] += 1
-    if dp[0] == 0:
-        return None
-    if dp[0] % 2 == 0:
+    for i in nums:
+        if i == 2:
+            x += 1
+        else:
+            y += 1
+    if x > y:
+        return "Maria"
+    if y > x:
         return "Ben"
-    return "Maria"
+    return None
