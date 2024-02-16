a8fb43f05912bba1e26d064fc1029e3b4de4e518
tobias-barakaa <ngairato@gmail.com>
2024-02-16 04:26:26 +0300

    this is the code

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python3
 """
-isWinner module
-"""
+Prime Game to check the prime numbers"""
 
 
 def isWinner(x, nums):
-    """ 
-    Prime Game
+    """Prime Game to check the prime numbers
+    Args:
+        x: the number of rounds
+        nums: the list of numbers
+    Returns:
+        the winner of the game
     """
-    if x < 1 or not nums:
+    if nums is None or x < 1:
         return None
     n = max(nums)
-    primes = [True for _ in range(n + 1)]
-    primes[0], primes[1] = False, False
+    primes = [True for i in range(n + 1)]
+    primes[0] = primes[1] = False
     for i in range(2, int(n ** 0.5) + 1):
         if primes[i]:
             for j in range(i * i, n + 1, i):
                 primes[j] = False
-    primes = [i for i, j in enumerate(primes) if j]
+    primes = [i for i, prime in enumerate(primes) if prime]
     primes = primes[1:]
     count = 0
     for i in nums:
