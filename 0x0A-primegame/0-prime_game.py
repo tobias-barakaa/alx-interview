6c90f7e994c23a0707050fce7098e98c77354456
tobias-barakaa <ngairato@gmail.com>
2024-02-16 03:29:09 +0300

    them brothers

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,29 +1,28 @@
 #!/usr/bin/python3
-"""
-Prime Game to check the prime numbers"""
-
+""" 
+    Prime Game"""
+    
 
 def isWinner(x, nums):
-    def is_prime(num):
-        if num < 2:
-            return False
-        for i in range(2, int(num ** 0.5) + 1):
-            if num % i == 0:
-                return False
-        return True
-
-    winners = []
-    for n in nums:
-        prime_count = sum(1 for num in range(2, n + 1) if is_prime(num))
-        
-        winners.append("Ben" if prime_count % 2 == 0 else "Maria")
-
-    maria_wins = winners.count("Maria")
-    ben_wins = winners.count("Ben")
-
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
+    """ Prime Game """
+    if not nums or x < 1:
         return None
+    n = max(nums)
+    sieve = [True] * (n + 1)
+    sieve[0:2] = [False, False]
+    for i in range(2, int(n ** 0.5) + 1):
+        if sieve[i]:
+            for j in range(i * i, n + 1, i):
+                sieve[j] = False
+    sieve = [i for i in range(n + 1) if sieve[i]]
+    m = len(sieve)
+    dp = [0] * (m + 1)
+    for i in range(m - 1, -1, -1):
+        dp[i] = dp[i + 1]
+        if sieve[i] in nums:
+            dp[i] += 1
+    if dp[0] == 0:
+        return None
+    if dp[0] % 2 == 0:
+        return "Ben"
+    return "Maria"
