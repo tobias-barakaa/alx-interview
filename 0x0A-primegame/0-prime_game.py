62e3199c09023b9bb24039fc20d18a0a287274de
tobias-barakaa <ngairato@gmail.com>
2024-02-16 03:42:54 +0300

    nums

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,14 +2,36 @@
 """ 
 isWinner module
 """
+
+
 def isWinner(x, nums):
-    """ 
-    isWinner function
     """
-    if x < 1:
+    Prime Game
+    """
+    if x < 1 or not nums:
         return None
-    if nums is None:
-        return None
-    if len(nums) < x:
-        return None
-    return "Maria"
+
+    n = max(nums)
+    primes = [False, False] + [True for i in range(n - 1)]
+    for i in range(2, int(n ** 0.5) + 1):
+        if primes[i]:
+            for j in range(i * i, n + 1, i):
+                primes[j] = False
+
+    primes = [i for i, j in enumerate(primes) if j]
+
+    m = 0
+    for i in nums:
+        while primes[m] < i:
+            m += 1
+        if primes[m] == i:
+            m += 1
+            x -= 1
+            if x == 0:
+                return "Maria"
+        else:
+            x -= 1
+            if x == 0:
+                return "Ben"
+    return "Ben"
+
